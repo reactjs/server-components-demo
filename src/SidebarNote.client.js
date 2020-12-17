daf42575c4d84aef116910153dc6e89f6f99ffb4
Lauren Tan <poteto@users.noreply.github.com>
2020-12-16 23:02:57 -0500

    [Demo] Set isExpanded when sidebar note is clicked

--- src/SidebarNote.client.js
+++ src/SidebarNote.client.js
@@ -6,15 +6,62 @@
  *
  */
 
-export default function SidebarNote({children}) {
+import {useState, unstable_useTransition} from 'react';
+
+import {useLocation} from './LocationContext.client';
+
+export default function SidebarNote({id, children, expandedChildren}) {
+  const [location, setLocation] = useLocation();
+  const [startTransition, isPending] = unstable_useTransition();
+  const [isExpanded, setIsExpanded] = useState(false);
+  const isActive = id === location.selectedId;
+
   return (
-    <div className="sidebar-note-list-item">
+    <div
+      className={[
+        'sidebar-note-list-item',
+        isExpanded ? 'note-expanded' : '',
+      ].join(' ')}>
       {children}
-      <button className="sidebar-note-open">Open note for preview</button>
-      <button className="sidebar-note-toggle-expand">
-        <img src="chevron-up.svg" width="10px" height="10px" alt="Expand" />
+      <button
+        className="sidebar-note-open"
+        style={{
+          backgroundColor: isPending
+            ? 'var(--gray-80)'
+            : isActive
+            ? 'var(--tertiary-blue)'
+            : '',
+          border: isActive
+            ? '1px solid var(--primary-border)'
+            : '1px solid transparent',
+        }}
+        onClick={() => {
+          startTransition(() => {
+            setLocation((loc) => ({
+              selectedId: id,
+            }));
+          });
+        }}>
+        Open note for preview
       </button>
-      <p className="sidebar-note-excerpt">Some excerpt</p>
+      <button
+        className="sidebar-note-toggle-expand"
+        onClick={(e) => {
+          e.stopPropagation();
+          setIsExpanded(!isExpanded);
+        }}>
+        {isExpanded ? (
+          <img
+            src="chevron-down.svg"
+            width="10px"
+            height="10px"
+            alt="Collapse"
+          />
+        ) : (
+          <img src="chevron-up.svg" width="10px" height="10px" alt="Expand" />
+        )}
+      </button>
+      {isExpanded && expandedChildren}
     </div>
   );
 }
--- src/SidebarNote.js
+++ src/SidebarNote.js
@@ -19,7 +19,11 @@ export default function SidebarNote({note}) {
     : format(updatedAt, 'M/d/yy');
   const summary = excerpts(marked(note.body), {words: 20});
   return (
-    <ClientSidebarNote id={note.id}>
+    <ClientSidebarNote
+      id={note.id}
+      expandedChildren={
+        <p className="sidebar-note-excerpt">{summary || <i>(No content)</i>}</p>
+      }>
       <header className="sidebar-note-header">
         <strong>{note.title}</strong>
         <small>{lastUpdatedAt}</small>
