d63000c066bc0b884ec070359d5ad1680d272b45
Jon Jaques <jon.jaques@alula.net>
2020-12-21 23:25:25 -0600

    Fixes docker-compose & pg initialization
     - ./scripts/init_db.sh is not executable (/bin/bash: bad interpreter: Permission denied)
     - same script fails because user is already created, only table creation is necessary.

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -2,16 +2,12 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE ROLE notesadmin WITH LOGIN PASSWORD 'password';
-    ALTER ROLE notesadmin WITH SUPERUSER;
-    ALTER DATABASE notesapi OWNER TO notesadmin;
-
-    DROP TABLE IF EXISTS notes;
-    CREATE TABLE notes (
-      id SERIAL PRIMARY KEY,
-      created_at TIMESTAMP NOT NULL,
-      updated_at TIMESTAMP NOT NULL,
-      title TEXT,
-      body TEXT
-    );
+  DROP TABLE IF EXISTS notes;
+  CREATE TABLE notes (
+    id SERIAL PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    title TEXT,
+    body TEXT
+  );
 EOSQL
