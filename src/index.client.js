897e95a42e70487cac7a8f4f74b3f7291bdd9648
Dan Abramov <dan.abramov@me.com>
2021-11-25 23:11:45 +0000

    This is a client app

--- src/index.client.js
+++ src/index.client.js
@@ -6,11 +6,13 @@
  *
  */
 
-import {hydrateRoot} from 'react-dom';
+import {createRoot} from 'react-dom';
 import Root from './Root.client';
 
 const initialCache = new Map();
-hydrateRoot(
-  document.getElementById('root'),
-  <Root initialCache={initialCache} />
-);
+const root = createRoot(document.getElementById('root'));
+root.render(<Root initialCache={initialCache} />);
+
+// Note: in this example, the initial page is rendered on the client.
+// However, the intended solution (which isn't built out yet) is to
+// have the server send the initial HTML, and hydrate from it.
